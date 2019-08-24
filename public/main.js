b1726c7de5daf9e450e188103656a9de4b188bb1
Ryan Sage <ryansage09@gmail.com>
2019-08-24 12:52:12 -0400

    removed test Url function

--- public/main.js
+++ public/main.js
@@ -5,9 +5,3 @@ function setActionUrl(){
     form.action = action_src;
 }
 
-
-function yourFunction(){
-    var action_src = "http://localhost/test/" + document.getElementsByName("keywords")[0].value;
-    var your_form = document.getElementById('your_form');
-    your_form.action = action_src ;
-}
