23f5a4a5c5da7db35d6a5e4e308ff3f3cffa6694
Ryan Sage <ryansage09@gmail.com>
2019-08-25 17:58:04 -0400

    testPrint function sucessfully iterates the array of value fields, and update vlaues for each field to have input value from score.

--- public/main.js
+++ public/main.js
@@ -5,13 +5,41 @@ function setActionUrl(){
     form.action = action_src;
 };
 
+const valueFields = [
+    'score', 'loaner', 'euchre', 'march', 'wins', 'loss'
+]
 function submitRound(){
     event.preventDefault();
+    let player = 'P1'
     var p1p2Score = parseFloat(document.getElementById('t1Score').value);
-    var p1Score = parseFloat(document.getElementById('p1Val1').value);
+    var p1Score = parseFloat(document.getElementById('score'+player).value);
     var total = 0;
     total += p1Score
     total += p1p2Score
-  document.getElementById('p1Val1').value = (total);
+  document.getElementById('score'+player).value = (total);
 };
+/*
+function valSelect(){
+let pValueSelect = p1Val
 
+for (let i = 0; i < 6; i++){
+pValue += i
+
+}
+}
+*/
+
+function testPrint (){
+    //event.preventDefault();
+    let player = 'P1'
+    for (i = 0; i<6; i++){
+        var total = 0;
+        var p1Score = parseFloat(document.getElementById('t1Score').value);
+        var p1p2Score = parseFloat(document.getElementById(valueFields[i]+player).value)
+        total += p1Score
+        total += p1p2Score
+        //console.log(valueFields[i]+player)
+       
+        document.getElementById(valueFields[i]+player).value = (total);
+    }
+}
